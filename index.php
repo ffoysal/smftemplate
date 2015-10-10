8e906aa9a0ad2841e7a2e11df0b6f98857a89c1a
Foysal Iqbal <foysal.iqbal.fb@gmail.com>
2015-10-10 00:46:17 -0400

    Basic Template for demo

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sabrian Memorial Foundation</title>
+    <link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/images/smf_icon.png" type="image/png"/>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="header">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/images/smflogo.png" class="img-circle">
+                    <span class="logo-title"><h1>Sabrina Memorial Foundation</h1></span>
+                </div>
+                <div class="col-md-6">
+                    <blockquote>
+                        <p>Success has nothing to do with what you gain in life or accomplish for yourself. It's what you do for others.</p>
+                        <footer><cite title="Source Title">Danny Thomas</cite></footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Navigation -->
+    <!--nav class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
+           
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"></a>
+            </div>
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+        
+    </nav -->
+
+    <!-- Page Content -->
+    <div class="container">
+		
+	<jdoc:include type="component" />
+        <hr>
+
+        <!-- Footer -->
+        <nav class="navbar navbar-default navbar-fixed-bottom">
+            
+        
+        <footer>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-5">
+                    Copyright &copy; Sabrina Memorial Foundation 2015
+                </div>
+            </div>
+        </footer>
+        </nav>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
