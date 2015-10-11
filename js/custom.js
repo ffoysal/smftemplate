4302bcac376dda88461f9a2f9fa5699cfe0a53ce
Asad <asad.islam@td.com>
2015-10-10 23:36:54 -0400

    Adding read more ellipsis style and removed jquery code

--- js/custom.js
+++ js/custom.js
@@ -10,24 +10,6 @@
 {
 	$(document).ready(function()
 	{
-		$('.readmore').each(function() {
-		    var $pTag = $(this).find('p');
-		    if($pTag.text().length > 83){
-		        var shortText = $pTag.text();
-		        shortText = shortText.substring(0, 83);
-		        $pTag.addClass('fullArticle').hide();
-		        shortText += '<a style="cursor:pointer" class="read-more-link"> ... Read more ></a>';
-		        $pTag.append('<a class="read-less-link" style="cursor:pointer"> &lt; Hide More</a>');
-		        $(this).append('<p class="preview">'+shortText+'</p>');
-		    }
-		});
-
-		$(document).on('click', '.read-more-link', function () {
-		    $(this).parent().hide().prev().show();
-		});
-
-		$(document).on('click', '.read-less-link', function () {
-		    $(this).parent().hide().next().show();
-		});
+		
 	})
 })(jQuery);
